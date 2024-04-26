7292c7fb0855c5487baeb49e2ab01fef81d89486
Sachin Nagila <sachinnagila2053@gmail.com>
2024-04-26 21:50:44 +1000

    Changing html contents

---
+++ script.js
@@ -0,0 +1,17 @@
+// const elm = document.getElementById("first");
+// const elm = document.getElementsByClassName("first");
+// const elm = document.getElementsByTagName("div");
+// const elm = document.querySelector(".first");
+// const elm = document.querySelectorAll(".first");
+
+// const elm = document.querySelector(".info");
+// // const value = elm.innerText;
+// const value = elm.innerHTML;
+// console.log(value);
+
+// document.write("hey just adding more content");
+
+const elm = document.querySelector(".message");
+// const val = elm.innerText;
+elm.innerText = "Hey jsut got text updated";
+// console.log(val);
