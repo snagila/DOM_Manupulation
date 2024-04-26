85cab9ac1b5c92c9f6d71acbbfb4d8c67ce0f9e7
Sachin Nagila <sachinnagila2053@gmail.com>
2024-04-26 22:05:34 +1000

    DOM manupulation

--- script.js
+++ script.js
@@ -11,7 +11,18 @@
 
 // document.write("hey just adding more content");
 
-const elm = document.querySelector(".message");
+// const elm = document.querySelector(".message");
 // const val = elm.innerText;
-elm.innerText = "Hey jsut got text updated";
+// elm.innerText = "Hey jsut got text updated";
 // console.log(val);
+
+const elm = document.querySelector("body");
+// const textNode = document.createTextNode("Hi this is dynamic content")
+// elm.appendChild(textNode);
+// console.log(elm);
+
+const paraElm = document.createElement("p");
+paraElm.innerText = "This is the dynamic text";
+// elm.appendChild(paraElm);
+const e1 = document.querySelector(".display");
+elm.insertBefore(paraElm, e1);
